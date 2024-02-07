ec1dffcab59f3a489d757c056d1ced45e6fef9af
dipesh kumar khadgi <d8khadgi@gmail.com>
2024-02-07 23:32:39 +0545

    first commit with theme

---
+++ android/app/src/main/kotlin/com/example/pasa_phone_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pasa_phone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:pasa_phone_app/utils/theme/theme.dart';
+
+import 'app.dart';
+
+/// -------- Entry point of Flutter App --------
+void main() {
+  runApp(const App());
+}
